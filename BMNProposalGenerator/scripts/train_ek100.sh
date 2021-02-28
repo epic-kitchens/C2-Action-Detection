e086c8bdde39251826dd9c477a79081b8746666c
Antonino Furnari <antonino.furnari@gmail.com>
2021-02-28 11:49:38 +0100

    Update train_ek100.sh

--- BMNProposalGenerator/scripts/train_ek100.sh
+++ BMNProposalGenerator/scripts/train_ek100.sh
@@ -1,2 +1,2 @@
 mkdir -p output/ek100
-python libs/bmn-fork/main.py data/ek100/ models/ek100/bmn/ output/ek100/ --path_to_video_features data/ek100/video_features/
+python main.py data/ek100/ models/ek100/bmn/ output/ek100/ --path_to_video_features data/ek100/video_features/
